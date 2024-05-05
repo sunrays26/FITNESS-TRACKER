90e613a5af97bcf6cd27a4a541300bc7204adf30
sunrays26 <168142797+sunrays26@users.noreply.github.com>
2024-05-06 03:22:05 +0530

    Add files via upload

---
+++ FitHome/server/index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+const createLog=(req,res,next)=>{
+  res.on("finish",function(){
+    console.log(req.method,decodeURI(req.url),res.statusCode,
+  res.statusMessage);
+  });
+  next();
+}
+
+app.use(createLog)
+app.use("/api/user/", UserRoutes);
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect("mongodb+srv://leo1207:leo%401207@cluster0.1shxgvw.mongodb.net/")
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
